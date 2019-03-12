105b945746461825fb9ce4a8da856e45fad86a01
sritunjungs <48472264+sritunjungs@users.noreply.github.com>
2019-03-12 15:43:12 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
-<?php
-    echo"<h1>Hello World</h1> <br/>";
-    
-    $text = "<table>
+
+<h1>Hello World</h1> <br/><table>
     <tr>
         <td>NIM</td>
         <td> : </td>
-        <td>16.01.53.0064</td>
+        <td>16.01.53.0143</td>
     </tr>
     <tr>
         <td>Nama</td>
         <td> : </td>
-        <td>Dimas Ahmad Rizal</td>
+        <td>Sri Tunjung Sujatmiko</td>
     </tr>
-</table> <br>";
-
-    echo $text;
-?>
-
-    <a href="/akademik">Go To Akademik</a>
+</table>
+<a href="https://unisbank.ac.id">Go To Akademik</a>
